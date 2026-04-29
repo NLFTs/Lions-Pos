56041e239e20dc55bad7e15edc3b8febc0716a75
salsaagustinanuriana-pixel <salsaagustinanuriana@gmail.com>
2026-04-29 20:31:25 +0700

    abcd

--- src/main/java/com/dak/spravel/model/Order_items.java
+++ src/main/java/com/dak/spravel/model/Order_items.java
@@ -2,10 +2,13 @@ package com.dak.spravel.model;
 
 import java.math.BigDecimal;
 
+import org.hibernate.validator.constraints.UUID;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
