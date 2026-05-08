03e3d4d9ee0bf0992ae823394f28f12158d9294c
dill444nrf-dot <dill444nrf@gmail.com>
2026-05-08 11:16:04 +0700

    validation procurement

--- src/main/java/com/dak/spravel/dto/request/partner/UpdatePartnerRequest.java
+++ src/main/java/com/dak/spravel/dto/request/partner/UpdatePartnerRequest.java
@@ -1,11 +1,19 @@
 package com.dak.spravel.dto.request.partner;
 
 import com.dak.spravel.model.common.Partners;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UpdatePartnerRequest {
+
+    @NotBlank(message = "Nama partner tidak boleh kosong")
     private String name;
+
+    @NotNull(message = "Plan tidak boleh kosong")
     private Partners.Plan plan;
+
+    @NotNull(message = "Status aktif tidak boleh kosong")
     private Boolean isActive;
 }
