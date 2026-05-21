0562f37c46dd9d74d63d61542f6dfc7cf39b9ddf
Miftah Aufar <miftahaufar@gmail.com>
2026-05-21 17:12:24 +0700

    fix: fix the error

--- src/main/java/com/dak/spravel/service/inventory/TransferRequestService.java
+++ src/main/java/com/dak/spravel/service/inventory/TransferRequestService.java
@@ -26,6 +26,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.dak.spravel.model.common.Partners;
 
 @Service
 @RequiredArgsConstructor
